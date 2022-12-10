f81e32be1ce60b2683fd202a5d397c8cff025c25
Diego Vieira <diegovieira.ads@gmail.com>
2022-12-10 14:17:32 -0300

    Criando uma função para o Binding

--- app/src/main/java/com/example/imc/MainActivity.kt
+++ app/src/main/java/com/example/imc/MainActivity.kt
@@ -12,36 +12,42 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar!!.hide()
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+
+        usarbinding()
 
         binding.button.setOnClickListener {
             calcular()
         }
 
     }
-    fun calcular(){
+
+    fun usarbinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    fun calcular() {
         val altura = binding.textAltura.text.toString().toDouble()
-        val peso  = binding.textPeso.text.toString().toDouble()
+        val peso = binding.textPeso.text.toString().toDouble()
 
         val resultado = peso / (altura * altura)
 
-        if(resultado <= 18.5){
+        if (resultado <= 18.5) {
             binding.textResultado.setText("O seu resultado é %.2f".format(resultado) + ", sua classificação é: Magreza")
             binding.imgResultado.setImageResource(R.drawable.magreza)
-        }else if(resultado >= 18.6 && resultado <= 24.9){
-            binding.textResultado.setText( "O seu resultado é %.2f".format(resultado) + ", sua classificação é: Normal")
+        } else if (resultado >= 18.6 && resultado <= 24.9) {
+            binding.textResultado.setText("O seu resultado é %.2f".format(resultado) + ", sua classificação é: Normal")
             binding.imgResultado.setImageResource(R.drawable.normal)
-        }else if(resultado >= 25 && resultado <= 29.9){
-            binding.textResultado.setText( "O seu resultado é %.2f".format(resultado) + ", sua classificação é: Sobrepeso (Obesidade I)")
+        } else if (resultado >= 25 && resultado <= 29.9) {
+            binding.textResultado.setText("O seu resultado é %.2f".format(resultado) + ", sua classificação é: Sobrepeso (Obesidade I)")
             binding.imgResultado.setImageResource(R.drawable.obesidade1)
-        }else if(resultado >= 30 && resultado <= 39.9){
-            binding.textResultado.setText( "O seu resultado é %.2f".format(resultado) + ", sua classificação é: Obesidade II")
+        } else if (resultado >= 30 && resultado <= 39.9) {
+            binding.textResultado.setText("O seu resultado é %.2f".format(resultado) + ", sua classificação é: Obesidade II")
             binding.imgResultado.setImageResource(R.drawable.obesidade2)
-        }else if(resultado >= 40) {
-            binding.textResultado.setText( "O seu resultado é %.2f".format(resultado) + ", sua classificação é: Obesidade III")
+        } else if (resultado >= 40) {
+            binding.textResultado.setText("O seu resultado é %.2f".format(resultado) + ", sua classificação é: Obesidade III")
             binding.imgResultado.setImageResource(R.drawable.obesidade3)
         }
-        }
     }
+}
